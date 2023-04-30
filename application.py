ca31050c00c973256857f8bc53bac54cca901e5a
piijey <pj.223f@gmail.com>
2023-05-01 02:39:33 +0900

    add piepie input&response

--- application.py
+++ application.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import response
 
 application = Flask(__name__)
 
 @application.route('/')
-def home():
-    return "Hello, AWS Elastic Beanstalk!"
+def main_handler():
+    print("main_handler -- ")
+    return render_template('index.html')
+
+@application.route('/piepie', methods=['POST'])
+def piepie_handler():
+    input_text = request.form['input_text']
+    print("PiePieHandler --", input_text)
+    piepie_text = "input:\t" + input_text + "\nresponse:\t" + response.piepie_response(input_text)
+    return piepie_text
 
 if __name__ == '__main__':
+    print("__main__ --")
     application.run(host='0.0.0.0', port=5000, debug=True)
